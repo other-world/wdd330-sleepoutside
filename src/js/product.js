1dc94c49a1f3f005cead198e08c0cf943fe878d2
robrib1820 <rribeiro1@byupathway.edu>
2025-11-22 20:44:33 -0300

    Expand inventory

--- src/js/product.js
+++ src/js/product.js
@@ -1,12 +1,11 @@
-import { getParam } from "./utils.mjs";
+import { getParam, loadHeaderFooter } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 import ProductDetails from "./ProductDetails.mjs";
 
-const productId = getParam("product");
+loadHeaderFooter();
 
 const dataSource = new ProductData("tents");
+const productID = getParam("product");
 
-const product = new ProductDetails(productId, dataSource);
-
+const product = new ProductDetails(productID, dataSource);
 product.init();
-
