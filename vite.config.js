8fb5ac8369e9843b043ea5f5ccddba18d7c9dde7
robrib1820 <rribeiro1@byupathway.edu>
2025-11-29 05:15:50 -0300

    updates and the build runned

--- vite.config.js
+++ vite.config.js
@@ -9,12 +9,16 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, "src/index.html"),
+
         productListing: resolve(__dirname, "src/product-listing.html"),
+
         cart: resolve(__dirname, "src/cart/index.html"),
-        // This is the correct path for individual product pages
+
         product: resolve(__dirname, "src/product_pages/index.html"),
+
         checkout: resolve(__dirname, "src/checkout/index.html"),
-        success: resolve(__dirname, "src/checkout/success.html"), // bonus – nice to have
+        
+        success: resolve(__dirname, "src/checkout/success.html"),
       },
     },
   },
