bd8bd48aef2599383fe786c4d3c7789e9f552321
other-world <jamie@other-world.org>
2026-06-13 22:42:14 -0600

    Week 03 team and individual activity stuff.

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,11 @@
 import ProductData from "./ProductData.mjs";
 import ProductList from "./ProductList.mjs";
+import { loadHeaderFooter } from "./utils.mjs";
 
 const category = "tents";
 const dataSource = new ProductData(category);
 const listElement = document.querySelector(".product-list");
 const productList = new ProductList(category, dataSource, listElement);
 
+loadHeaderFooter();
 productList.init();
--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -29,10 +29,34 @@ export function getParam(param){
 }
 
 export function renderListWithTemplate(templateFn, parentElement, list, position, clear) {
-  if (clear == true){
+  if (clear){
     parentElement.innerHTML = ``;
   }
   list.forEach(item => {
   parentElement.innerHTML += templateFn(item);
   });
+}
+
+export function renderWithTemplate(template, parentElement, data, callback) {
+  parentElement.innerHTML += template;
+  if (callback) {
+    callback(data);
+  }
+}
+
+export async function loadTemplate(path) {
+  const response = await fetch(path);
+  const template = await response.text();
+  return template;
+}
+
+export async function loadHeaderFooter(){
+  const headerTemplate = await loadTemplate("../partials/header.html");
+  const headerElement = document.querySelector("#main-header");
+  renderWithTemplate(headerTemplate, headerElement);
+  
+  const footerTemplate = await loadTemplate("../partials/footer.html");
+  const footerElement = document.querySelector("#main-footer");
+  renderWithTemplate(footerTemplate, footerElement);
+
 }
